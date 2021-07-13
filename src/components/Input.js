601b8c92b761c465eb3b27b741b58f754129e71c
nichro02 <nichro02@gmail.com>
2021-07-12 19:28:28 -0600

    Created form on InputTodo component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,16 @@
+import { Fragment } from 'react'
+
 const InputTodo = () => {
 
-    return <h1>Input Todo</h1>
+    return(
+        <Fragment>
+            <h1 className='text-center mt-5'>Input Todo</h1>
+            <form className='d-flex mt-5'>
+                <input type='text' className='form-control'/>
+                <button className='btn btn-success'>Add</button>
+            </form>
+        </Fragment>
+    ) 
 }
 
 export default InputTodo
