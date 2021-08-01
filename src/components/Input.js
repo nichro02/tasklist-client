f5ca1304557765cd79cdf383a17a118ab7b5f5fa
nichro02 <nichro02@gmail.com>
2021-08-01 15:51:06 -0600

    Added useState to capture form entry, wrote function to handle form submit

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,39 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 const InputTodo = () => {
+
+    //capture todo being entered in form in state
+    const [description, setDescription] = useState('')
+
+    //handle form submit
+    const onSubmit = async e => {
+        e.preventDefault()
+        try {
+            const body = {description}
+            const response = await fetch('http://localhost:5000/todos', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(body)
+            })
+
+            console.log(response)
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
 
     return(
         <Fragment>
             <h1 className='text-center mt-5'>Input Todo</h1>
-            <form className='d-flex mt-5'>
-                <input type='text' className='form-control'/>
+            <form 
+                className='d-flex mt-5'
+                onSubmit={onSubmit}
+            >
+                <input 
+                    type='text' className='form-control'
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
                 <button className='btn btn-success'>Add</button>
             </form>
         </Fragment>
