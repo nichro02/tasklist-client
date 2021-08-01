7b1ed312698fc99b0bc3fed87f73ec9b05d88d91
nichro02 <nichro02@gmail.com>
2021-08-01 16:20:00 -0600

    Prepped ListTodo table that will display list of items

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //components
 import InputTodo from './components/Input'
+import ListTodos from './components/ListTodo'
 
 function App() {
   return (
     <Fragment>
       <div className='container'>
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
---
+++ src/components/ListTodo.js
@@ -0,0 +1,36 @@
+import { Fragment } from 'react'
+
+const ListTodos = () => {
+    return(
+        <Fragment>
+            <table class="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+        </Fragment>
+    )
+}
+
+export default ListTodos
