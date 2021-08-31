e5e55ac2cf79e4ee8e43a3fd1ac56b9b44c1a426
nichro02 <nichro02@gmail.com>
2021-08-30 19:17:43 -0600

    Refactored EditTodo to be a modal where user can edit text to update a task

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,10 +1,57 @@
-
+import { Fragment } from 'react'
 
 const EditTodo = () => {
     return(
-        <h1>
-            Edit Todo
-        </h1>
+        <Fragment>
+            <button
+                type='button'
+                className='btn btn-warning'
+                data-bs-toggle='modal'
+                data-bs-target='#myModal'
+            >
+                Edit
+            </button>
+
+            <div className='modal' id='myModal'>
+                <div className='modal-dialog'>
+                    <div className='modal-content'>
+                        <div className='modal-header'>
+                            <h4 className='modal-title'>Edit Todo</h4>
+                            <button 
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            >
+                            </button>
+                            
+                        </div>
+
+                        <div className='modal-body'>
+                            <input type='text' className='form-control'/>
+                        </div>
+
+                        <div className='modal-footer'>
+                        <button
+                                type='button'
+                                className= 'btn btn-warning'
+                                data-dismiss='modal'
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type='button'
+                                className= 'btn btn-danger'
+                                data-dismiss='modal'
+                                data-bs-dismiss='modal'
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
